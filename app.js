13bc6c2803357e0140df5a6462301550b2e5dfac
lijianglin <lijianglincqps@163.com>
2020-09-26 19:20:04 +0800

    第一次提交

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const bodyParser= require('body-Parser')
 const proRouter = require('./router/pro.js')
 // 创建web服务器
 const app = express();
+// 解决本地跨域问题
+app.all('*', function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");//项目上线后改成页面的地址
+	res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+	next();
+})
 // 设置端口
 app.listen(8080);
 // 托管静态资源
